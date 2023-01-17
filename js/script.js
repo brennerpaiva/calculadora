7c4e133ca787cd7c4dbd8bc55adf88fdc60db64a
brennerpaiva <114958953+brennerpaiva@users.noreply.github.com>
2023-01-17 14:09:51 -0300

    html e css

---
+++ js/script.js
@@ -0,0 +1,6 @@
+ previousOT = document.getElementById("previous-operation")
+
+const currentOT = document.querySelector('#current-operation')
+const buttons = document.querySelectorAll("#buttons-container button")
+
+console.log(buttons)feit
